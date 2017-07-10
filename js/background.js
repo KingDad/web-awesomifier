012ad8d21a66d5cc3ba4d3a6e114d619cf3d8afb
KingDad <pgrcarlson@gmail.com>
2017-07-10 15:50:57 -0400

    Working on text replacement.

--- js/background.js
+++ js/background.js
@@ -1,5 +1,8 @@
+/*var codeAlert = "alert('Ready to PARTY')"*/
+var codeAlert = "var elements = document.getElementsByTagName('*');console.log('something happened');for (i = 0; i < elements.length; i++){var toReplace = /\bthe\b/gi;elements[i].innerHTML.replace(toReplace, 'a');console.log(elements[i].innerHTML);}";
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    code: 'alert("Ready to ROLL")'
+    code: codeAlert
   });
 });
